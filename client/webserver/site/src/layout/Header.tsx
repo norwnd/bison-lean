76fadeb8420d21febbe1e61dfbab88c862a2eaf1
norwnd <norwnd>
2026-04-16 13:57:25 +0300

    improve navigation

--- client/webserver/site/src/layout/Header.tsx
+++ client/webserver/site/src/layout/Header.tsx
@@ -1,35 +1,43 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import { useAuthStore } from '../stores/useAuthStore'
 import { ROUTES } from '../router/routes'
 
+function navCls (path: string, current: string): string {
+  const active = current.startsWith(path) ? ' active' : ''
+  return `navlink hoverbg d-flex align-items-center${active}`
+}
+
 export function Header () {
   const { t } = useTranslation()
+  const { pathname } = useLocation()
   const authed = useAuthStore(s => s.authed)
   const exchanges = useAuthStore(s => s.exchanges)
   const hasConnection = Object.keys(exchanges).length > 0
+
+  const settingsPath = authed ? ROUTES.SETTINGS : ROUTES.LOGIN
 
   return (
     <header id="header" className="d-flex align-items-center justify-content-between border-bottom">
       {/* Left side: portal slot for page-specific header content (e.g. market stats) */}
       <div id="headerSlot" className="d-flex align-items-center flex-grow-1 overflow-hidden" />
 
-      <div className="mainlinks fs18 pe-2 text-nowrap d-flex h-100">
+      <div className="mainlinks fs18 text-nowrap d-flex">
         {authed && (
-          <Link to={ROUTES.WALLETS} className="demi hoverbg d-flex align-items-center">
+          <Link to={ROUTES.WALLETS} className={`demi ${navCls(ROUTES.WALLETS, pathname)}`}>
             {t('Wallet')}
           </Link>
         )}
         {authed && hasConnection && (
-          <Link to={ROUTES.MARKETS} className="demi hoverbg d-flex align-items-center">
+          <Link to={ROUTES.MARKETS} className={`demi ${navCls(ROUTES.MARKETS, pathname)}`}>
             {t('Trade')}
           </Link>
         )}
         {authed && hasConnection && (
-          <Link to={ROUTES.MM} className="ico-robot lh1 fs32 hoverbg d-flex align-items-center" />
+          <Link to={ROUTES.MM} className={`ico-robot lh1 fs32 ${navCls(ROUTES.MM, pathname)}`} />
         )}
-        <Link to={authed ? ROUTES.SETTINGS : ROUTES.LOGIN} className="d-flex align-items-center hoverbg position-relative pointer">
-          <span className="ico-hamburger fs20 p-2" />
+        <Link to={settingsPath} className={navCls(settingsPath, pathname)}>
+          <span className="ico-hamburger fs20" />
         </Link>
       </div>
     </header>
