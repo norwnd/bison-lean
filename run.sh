da322f65e987038e7d551a2886cb6c499e0cdad8
norwnd <norwnd>
2026-04-25 21:28:27 +0300

    cleanup

--- run.sh
+++ run.sh
@@ -28,8 +28,5 @@ fi
 
 ## mainnet with dedicated DB:
 #(./client/cmd/bisonw/bisonw --db=/Users/norwnd/d-e-x-c-db/mainnet/dexc.db --webaddr=127.0.0.1:3333 --log=trace --httpprof)
-## testnet
+## testnet with dedicated DB:
 #(./client/cmd/bisonw/bisonw --db=/Users/norwnd/d-e-x-c-db/testnet/dexc.db --webaddr=127.0.0.1:3333 --log=trace --testnet --httpprof)
-
-## mainnet with disaster backups:
-#(./client/cmd/bisonw/bisonw --webaddr=127.0.0.1:3333 --log=trace --skynetapikey=I8JMMBPUNMEHGCV056IG1V8OEEIB0CJUTG97S0J3IQNJJ4CSGH0G --skynetapiurl='https://web3portal.com' --testnet --httpprof)
